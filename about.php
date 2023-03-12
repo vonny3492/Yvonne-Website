36240e22b3cbc6399d0770e56b468497fc01fce1
vonny3492 <62268930+vonny3492@users.noreply.github.com>
2023-03-12 13:08:22 -0500

    Adding About Me File

---
+++ about.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/yew.css" />
+		<link rel="stylesheet" href="css/grid.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="images/favicon.png" href="favicon.ico" />
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Josefin+Sans:600,700, 600italic' rel='stylesheet' type='text/css'>
+		
+		<title>Yew: Yvonne R Muller</title>
+	</head>
+		
+	<body>
+
+		<?php include 'header.php';?>
+		
+		<div id="about" class="section group">
+			
+			<div class="col span_1_of_2">
+				<div class="pic">
+					<img src="images/hobbiton.jpg" title="Thank you for your time!" style="width: 90%; padding-left: 4%; margin: 0; margin-top: 3%;"/>
+				</div>
+			</div>
+			
+			<div class="col span_1_of_2">
+				<div class="writ">
+					<p> Hello! I'm Yvonne, a user experience designer with a strong research and web focus.</p>
+					<p> While I grew up in Madison I went to the University of Minnesota to earn my BFA and spend a wonderful semester in Scotland. Since moving back home I’ve gained professional experience in usability and graphic design all while caring for my frolicsome ferrets and delightful doggo.</p>
+					
+					<div style="display: flex; overflow: hidden; align-items: center; justify-content: center;">
+					<img src="images/ferrets.jpg" title="Momo and Oogi" style="padding: 0px; width: 200px;"/> <img src="images/appa.jpg" title="Appa" style="padding: 0px; margin-left: 5%; width: 200px;"/>
+					</div>
+					
+					<p>I'm passionate about problem solving in ways that work best for everyone. I think usability research, precise layout, and smart typography are key to great design.</p>
+					
+					<p>I'm keen about reading, goat farms, pottery, television, organizing, and work life balance. </p>
+					
+					
+				
+				
+					<p class="link">Check out my résumé for more information</p>
+				
+					<a class="button" style="margin: auto; margin-bottom: 30px;" href="images/YvonneRMuller_Resume.pdf"><p>résumé</p></a> 
+				
+				</div>
+			</div>
+		</div>	
+		
+		<?php include 'footer.php';?>
+				
+	</body>	
+
+</html>
